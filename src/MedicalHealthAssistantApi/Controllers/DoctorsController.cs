fee96ea74d25b44032bfa139ae6be5bdc1574990
Avazbek0704 <siddikovavazbek213@gmail.com>
2023-09-07 20:17:22 +0500

    DoctorController added

---
+++ src/MedicalHealthAssistantApi/Controllers/DoctorsController.cs
@@ -0,0 +1,60 @@
+﻿using MedicalHealthAssistantApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Service.DTOs.Doctors;
+using Service.Interfaces;
+
+namespace MedicalHealthAssistantApi.Controllers;
+
+public class DoctorsController : BaseController
+{
+    private readonly IDoctorService doctorService;
+    public DoctorsController(IDoctorService doctorService)
+    {
+        this.doctorService = doctorService;
+    }
+
+    [HttpPost("create")]
+    public async Task<IActionResult> AddAsync(DoctorCreationDto dto)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await this.doctorService.CreateAsync(dto)
+        });
+
+    [HttpPut("update")]
+    public async Task<IActionResult> UpdateAsync(DoctorUpdateDto dto)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await this.doctorService.UpdateAsync(dto)
+        });
+
+    [HttpDelete("delete/{id:long}")]
+    public async Task<IActionResult> DeleteAsync(long id)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await this.doctorService.DeleteAsync(id)
+        });
+
+    [HttpGet("get/{id:long}")]
+    public async Task<IActionResult> GetByIdAsync(long id)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await this.doctorService.GetAsync(id)
+        });
+
+    [HttpGet("get-all")]
+    public async Task<IActionResult> GetAllsync()
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = doctorService.GetAllDoctorsAsync()
+        });
+}
