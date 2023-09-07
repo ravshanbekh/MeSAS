e3dee4c1e30510ed311f5b34d8557a663dde683f
Abboskhan3 <abbosnewz1@gmail.com>
2023-09-07 16:42:36 +0300

    add interfaces

---
+++ src/Service/Interfaces/IHospitalService.cs
@@ -0,0 +1,13 @@
+﻿using Service.DTOs.Hospitals;
+using Service.DTOs.Users;
+
+namespace Service.Interfaces;
+
+public interface IHospitalService
+{
+    Task<HospitalResultDto> CreateAsync(HospitalCreationDto dto);
+    Task<HospitalResultDto> UpdateAsync(HospitalUpdateDto dto);
+    Task<bool> DeleteAsync(long id);
+    Task<IEnumerable<HospitalResultDto>> GetAllHospitalsAsync();
+    Task<HospitalResultDto> GetAsync(long Id);
+}
---
+++ src/Service/Interfaces/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using Service.DTOs.Users;
+
+namespace Service.Interfaces;
+
+public interface IUserService
+{
+    Task<UserResultDto> CreateAsync(UserCreationDto dto);
+    Task<UserResultDto> UpdateAsync(UserUpdateDto dto);
+    Task<bool> DeleteAsync(long id);
+    Task<IEnumerable<UserResultDto>> GetAllUsersAsync();
+    Task<UserResultDto> GetAsync(long Id);
+}
