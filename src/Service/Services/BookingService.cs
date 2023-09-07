7e569d825224d6f3f9de2b5a41836272b88b5d6e
RavshanbekIsroilov <isroilovravshan42@gmail.com>
2023-09-07 18:33:17 +0500

    add services

---
+++ src/Service/Services/BookingService.cs
@@ -0,0 +1,5 @@
+﻿namespace Service.Services;
+
+public class BookingService
+{
+}
---
+++ src/Service/Services/UserService.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using DAL.IRepositories;
+using Domain.Entities;
+using Service.DTOs.Users;
+using Service.Exceptions;
+
+namespace Service.Services;
+
+public class UserService
+{
+
+}
