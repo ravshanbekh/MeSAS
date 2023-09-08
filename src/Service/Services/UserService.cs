e5416a70bd3a94d0debd460d1779c88b494a695b
Abboskhan3 <abbosnewz1@gmail.com>
2023-09-08 13:24:44 +0300

    add

--- src/Service/Services/UserService.cs
+++ src/Service/Services/UserService.cs
@@ -10,6 +10,8 @@ using Service.Exceptions;
 using Service.Extensions;
 using Service.Helpers;
 using Service.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Service.Services;
 
@@ -51,7 +53,7 @@ public class UserService : IUserService
 
     public async Task<IEnumerable<UserResultDto>> GetAllUsersAsync()
     {
-        var users = await this.repository.SelectAll()
+        var users = await repository.SelectAll()
            .ToListAsync();
            //.ToPaginate(@params)
         var result = this.mapper.Map<IEnumerable<UserResultDto>>(users);
